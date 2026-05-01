af838ca24cd853294957a7dd28816c97a006d5d1
Nosheen Ullah <nosheen.u.16@gmail.com>
2026-05-01 00:13:03 -0700

    asgn 3.0

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
